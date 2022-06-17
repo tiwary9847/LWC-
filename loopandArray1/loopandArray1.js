5277baab4a82dd4c90f44f801aa6b918838a830b
Rupesh <tiwary9847@gmail.com>
2022-06-17 23:17:56 +0530

    Loop and array with lwc for each and iteration

---
+++ loopandArray1/loopandArray1.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class LoopandArray1 extends LightningElement {
+    cities=['mum','pune','hyderbad','bhopal','indore'];
+}
---
+++ loopandArray2/loopandArray2.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class LoopandArray2 extends LightningElement {
+    evenum=[1,3,2,4,2,4,5,6,2,8,32];
+    Student=[{'Name':'Rupesh','Age':30,'cities':'pune','salary':50000}]
+}
